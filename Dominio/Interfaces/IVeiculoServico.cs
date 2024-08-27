8413bf86672538df48fb890415f461e6302fdcf9
SILVIO DIAS <sdferreira@sad.ms.gov.br>
2024-08-27 11:18:07 -0400

    Implementado modelo Veiculo

---
+++ Dominio/Interfaces/IVeiculoServico.cs
@@ -0,0 +1,22 @@
+using MininalApi.Dominio.Entidades;
+using MininalApi.DTOs;
+
+namespace MininalApi.Dominio.Interfaces;
+
+public interface IVeiculoServico
+{
+        list<Veiculo> Todos(
+                int pagina = 1, 
+                string? nome = null, 
+                string? marca = null);
+
+        Veiculo? BuscaPorId(int id);
+
+        void Incluir(Veiculo veiculo);  
+
+        void Atualizar(Veiculo veiculo);
+
+        void Apagar(Veiculo veiculo);
+
+
+}
---
+++ Dominio/Servicos/VeiculoServico.cs
@@ -0,0 +1,58 @@
+using MininalApi.Dominio.Entidades;
+using MininalApi.Dominio.Interfaces;
+using MininalApi.DTOs;
+using MininalApi.Infraestrutura.Db;
+
+namespace MininalApi.Dominio.Servicos;
+
+public class VeiculoServico : IVeiculoServico
+{
+
+    private readonly DbContexto _contexto;
+
+    public VeiculoServico(DbContexto contexto){
+        _contexto = contexto;
+    }
+
+   
+
+    public List<Veiculo> Todos(
+        int pagina = 1,
+        string? nome = null,
+        string? marca = null){
+            
+            var query = _contexto.Veiculo.AsQueryble();
+
+            if(!string.IsNullOrEmpty(nome)){
+                query = query.where(v => v.Nome.ToLower().Contains(nome));
+            }
+
+            int itensPorPagina = 10;
+
+            query = query
+                        .Skip((pagina -1) * itensPorPagina)
+                        .Take(itensPorPagina);
+                        
+            return query.ToList();
+        }
+
+    public Veiculo? BuscaPorId(int id){
+        return _contexto.Veiculo.FirstOrDefault(v => v.Id == id);
+    }
+
+    public void Incluir(Veiculo veiculo){
+        _contexto.Veiculo.Add(veiculo);
+        _contexto.SaveChanges();
+    }
+
+    public void Atualizar(Veiculo veiculo){
+        _contexto.Veiculo.Update(veiculo);
+        _contexto.SaveChanges();
+    }
+
+     public void Apagar(Veiculo veiculo){
+        _contexto.Veiculo.Remove(veiculo);
+        _contexto.SaveChanges();
+    }
+   
+}
