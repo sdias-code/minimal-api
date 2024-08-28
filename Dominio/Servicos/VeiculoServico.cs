231ecda8307e0a69c7af9b0f992bf751b7747394
SILVIO DIAS <sdferreira@sad.ms.gov.br>
2024-08-28 07:47:55 -0400

    Criado endpoint Get de veículos

--- Dominio/Servicos/VeiculoServico.cs
+++ Dominio/Servicos/VeiculoServico.cs
@@ -15,7 +15,7 @@ public class VeiculoServico : IVeiculoServico
     }   
 
     public List<Veiculo> Todos(
-        int pagina = 1,
+        int? pagina = 1,
         string? nome = null,
         string? marca = null){
             
@@ -27,9 +27,11 @@ public class VeiculoServico : IVeiculoServico
 
             int itensPorPagina = 10;
 
-            query = query
-                        .Skip((pagina -1) * itensPorPagina)
+            if(itensPorPagina != null){
+                query = query
+                        .Skip(((int) pagina -1) * itensPorPagina)
                         .Take(itensPorPagina);
+            }            
                         
             return query.ToList();
         }
