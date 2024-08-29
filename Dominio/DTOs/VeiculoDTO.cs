c40259f8a9e6138cafd9c2fcfb1a30a16500137f
sdias-code <silviodias.ms@gmail.com>
2024-08-29 01:26:27 -0400

    efetuado migration da entidade veiculo

--- Dominio/DTOs/VeiculoDTO.cs
+++ Dominio/DTOs/VeiculoDTO.cs
@@ -1,13 +1,18 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace MininalApi.DTOs;
 public record VeiculoDTO
 {
     
     [StringLength(150)]
+    [Required]
     public string  Nome { get; set; } = default!;
 
     [StringLength(100)]
+    [Required]
     public string Marca { get; set; } = default!;
 
+    [Required]
     public int Ano { get; set; } = default!;
 }
