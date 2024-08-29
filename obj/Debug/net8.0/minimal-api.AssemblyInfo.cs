bd1ee8e5dc2c05c3fe5fb425c7aa843756a7795d
sdias-code <silviodias.ms@gmail.com>
2024-08-28 22:19:05 -0400

    refactor assembly info

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e71d931a590f8d35d07fa3d788fbf7cbd9eb4c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a9e487e76ae23f1d8eb89e5cdab1077c0ccf5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
