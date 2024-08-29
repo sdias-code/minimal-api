ab047dc053fd138cef1584c928f098f94085a5b3
sdias-code <silviodias.ms@gmail.com>
2024-08-28 22:20:19 -0400

    AssemblyInfo net8.0

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a9e487e76ae23f1d8eb89e5cdab1077c0ccf5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1ee8e5dc2c05c3fe5fb425c7aa843756a7795d")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
